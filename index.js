47085d7e2c4e007c6f1b152556eb1216a00c42bf
Pethyy97 <99269416+Pethyy97@users.noreply.github.com>
2023-03-18 18:51:54 +0100

    Add files via upload

---
+++ index.js
@@ -0,0 +1,26 @@
+const Discord = require("discord.js");
+const tokenfile = require("./tokenfile.json");
+const botconfig = require("./botconfig.json");
+const bot = new Discord.Client({disableEveryone: true});
+
+bot.on("ready", async() => {
+    console.log(`${bot.user.username} elindult!`)
+
+    let státuszok = [
+        "Készítő: Mageth",
+        "Üdv AveetGaming szerven"
+    ]
+
+    setInterval(function() {
+        let status = státuszok[Math.floor(Math.random()* státuszok.length)]
+
+        bot.user.setActivity(status, {type: "WATCHING"})
+    }, 3000)
+})
+
+
+
+
+
+
+bot.login(tokenfile.token);
